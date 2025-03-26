ffd694b08c72857e0cba9908412d72bd0c1a67b8
Jason the noob <jason-ollivierre@hotmail.com>
2025-03-26 13:57:12 +0000

    min 1.22, trabajando en el player

---
+++ spotify-clon/src/components/Greeting.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    const currentTime = new Date()
+    const CurrentHour = currentTime.getHours()
+
+    let greeting = ""
+    if (CurrentHour < 12) {
+        greeting = "Good Morning"
+    } else if (CurrentHour < 18) {
+        greeting = "Good Afternoon"
+    } else {
+        greeting = "Good Evening"
+    }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
